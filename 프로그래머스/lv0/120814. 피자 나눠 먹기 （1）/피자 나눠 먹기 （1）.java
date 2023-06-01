6bb0048d778fe9562dfaa39681087d1e2011959d
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2023-06-01 18:18:09 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 67.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if (n % 7 != 0) {
+			answer += 1;
+			answer += n / 7;
+		} else {
+			answer += n / 7;
+		}
+        return answer;
+    }
+}
