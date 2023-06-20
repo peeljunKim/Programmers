7fc6f8e672ee5333a7119223602acd81a5242365
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2023-06-20 22:20:31 +0900

    [unrated] Title: 5명씩, Time: 0.26 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String[] solution(String[] names) {
+        String[] answer = new String[(int) Math.ceil(names.length / 5.0)];
+
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = names[i * 5];
+		}
+		return answer;
+    }
+}
