751fb5f7dd2e3ce4b23bd43b87d034ed85c65274
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2023-07-01 19:11:46 +0900

    [unrated] Title: ad 제거하기, Time: 0.98 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
@@ -1,17 +1,15 @@
 import java.util.ArrayList;
 class Solution {
     public String[] solution(String[] strArr) {
-        int count = 0;
-		ArrayList<String> idxList = new ArrayList<String>();
+       ArrayList<String> idxList = new ArrayList<String>();
 		for (int i = 0; i < strArr.length; i++) {
 			if (strArr[i].contains("ad")) {
 			} else {
-				idxList.add(count, strArr[i]);
-				count++;
+				idxList.add(strArr[i]);
 			}
 		}
 
-		String[] answer = new String[count];
+		String[] answer = new String[idxList.size()];
 		for (int i = 0; i < answer.length; i++) {
 			answer[i] = idxList.get(i);
 		}
