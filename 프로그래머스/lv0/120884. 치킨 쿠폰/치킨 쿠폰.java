ad742f64bd3ea86ea5da5c2441c148a9a9921e0c
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2023-07-04 09:00:42 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -2,7 +2,6 @@ class Solution {
     public int solution(int chicken) {
 		int answer = 0;
 		int rest = 0;
-		// chichen == 쿠폰
 		while (chicken >= 10) {
 			answer += chicken / 10; // 서비스 치킨 수
 			rest = chicken % 10; // 서비스 치킨을 시키고 남은 쿠폰
