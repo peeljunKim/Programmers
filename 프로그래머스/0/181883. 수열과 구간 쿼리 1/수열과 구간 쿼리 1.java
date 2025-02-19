84a3b9fde376022f950f8fe5fe09f3680e3441c7
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2025-02-20 00:05:56 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 1.71 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        
+        for(int i=0; i<queries.length; i++){
+            int s = queries[i][0];
+            int e = queries[i][1];
+            
+            for(int j = s; j<=e; j++){
+                arr[j]++;
+            }
+            
+        }
+        
+        return arr;
+    }
+}
