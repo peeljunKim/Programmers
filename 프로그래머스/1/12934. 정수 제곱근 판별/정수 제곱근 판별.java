c9e336ed1b66de4dfe82b0dc66a60eaa52e2a7b3
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2025-03-15 22:04:34 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.07 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        double temp = Math.sqrt(n);
+        if(temp % 1 == 0) {
+            answer = (long) Math.pow(temp + 1, 2);
+        }else {
+            answer = -1;
+        }
+        return answer;
+    }
+}
