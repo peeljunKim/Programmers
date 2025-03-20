055cce5842d1bf6e09f53dc5890a4a8356026dae
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2025-03-20 23:19:55 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.14 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,17 +1,24 @@
 class Solution {
     public boolean solution(String s) {
-        boolean answer = true;
-        try{
-            if(s.length() == 4 || s.length() == 6){
-                Integer.parseInt(s);
-                return true;
-            }
-            return false;
-        }catch(NumberFormatException e){
-            return false;
+        boolean answer = false;
+        if(s.length() == 4 || s.length() == 6){
+            answer = s.matches("(^[0-9]*$)");
         }
-
+        
+        
+        // try{
+        //     if(s.length() == 4 || s.length() == 6){
+        //         Integer.parseInt(s);
+        //         return true;
+        //     }
+        //     return false;
+        // }catch(Exception e){
+        //     return false;
+        // }
+        
+        return answer;
     }
 }
-// 문자 길이 4 or 6
-// 숫자로만 이루어지면 ture
+
+// 다른 사람의 풀이를 봤는데 try-catch문으로 풀면 안된다고 한다. 왜냐하면
+// 예외는 실제 예외 상황일때만 사용해야 되고, 예외 처리는 리소스가 많이 들어서
