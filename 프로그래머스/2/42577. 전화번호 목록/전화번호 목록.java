b0a0856cb34641ad4a560daafc00910521e7528b
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2025-05-20 18:59:21 +0900

    [level 2] Title: 전화번호 목록, Time: 273.62 ms, Memory: 214 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    /**
+    @param phone_book 전화번호 배열
+    **/
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Set<String> set = new HashSet<>();
+        for(String str : phone_book){
+            set.add(str);
+        }
+        
+        for(String phoneNumber : phone_book){
+            for (int i = 1; i < phoneNumber.length(); i++) {
+                String prefix = phoneNumber.substring(0, i);
+
+                if (set.contains(prefix)) {
+                    return false;
+                }
+            }
+        }
+        
+        // if(phone_book[i + 1].startsWith(str, 0)) return false;
+            
+                
+        return answer;
+    }
+}
+// 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true
+// 번호 중복 x
+// 번호 길이 1~20
+// phone_book 1 ~ 1000000
+// 효율성 테스트
