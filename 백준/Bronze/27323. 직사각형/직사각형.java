067972626e97563a5f06e2546e39d910a0f1f431
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2026-01-01 20:35:20 +0900

    [Bronze V] Title: 직사각형, Time: 172 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row = sc.nextInt(); 
+        int col = sc.nextInt(); 
+
+        System.out.println(row * col); 
+
+        sc.close();
+    }
+}
