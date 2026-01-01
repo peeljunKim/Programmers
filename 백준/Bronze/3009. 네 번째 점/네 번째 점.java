2141a0d2bb3e2127da5fa77b0593c30914f10da9
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2026-01-01 20:49:39 +0900

    [Bronze III] Title: 네 번째 점, Time: 188 ms, Memory: 18408 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        
+        int x3 = sc.nextInt();
+        int y3 = sc.nextInt();
+
+        int x4 = 0;
+        int y4 = 0;
+
+        // 같은 값이 2번씩 나와야 하므로, 한 번만 나온거 찾기
+        if (x1 == x2) {
+            x4 = x3;
+        } else if (x1 == x3) {
+            x4 = x2;
+        } else {
+            x4 = x1;
+        }
+
+        if (y1 == y2) {
+            y4 = y3;
+        } else if (y1 == y3) {
+            y4 = y2;
+        } else {
+            y4 = y1;
+        }
+
+        System.out.println(x4 + " " + y4);
+
+        sc.close();
+    }
+}
